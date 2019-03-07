a0866b102c8325466baf05d6ad3dff907cb2fd28
JananiVelmurugan <jananise@gmail.com>
2019-03-07 15:16:59 +0530

    implemented delete book

--- src/com/chainsys/dao/DeleteDAO.java
+++ src/com/chainsys/dao/DeleteDAO.java
@@ -6,20 +6,27 @@ import org.hibernate.SessionFactory;
 import com.chainsys.model.Book;
 import com.chainsys.util.HibernateUtil;
 
-public class UpdateDAO {
+public class DeleteDAO {
+
 	public static void main(String[] args) {
+
 		// Create session factory object
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		// getting session object from session factory
 		Session session = sessionFactory.openSession();
 		// getting transaction object from session object
 		session.beginTransaction();
-
-		Book book = (Book) session.get(Book.class, 1);
-		book.setName("Python");
-		System.out.println("Updated Successfully");
+		Book book = (Book) session.get(Book.class, 4);
+		if (book != null) {
+			System.out.println("Deleted Successfully");
+			session.delete(book);
+		}
+		else{
+			System.out.println("No Record Found");
+		}
 		session.getTransaction().commit();
 		sessionFactory.close();
+
 	}
 
 }
