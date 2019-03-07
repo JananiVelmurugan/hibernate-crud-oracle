435a1162ace87dfaf7211f7e44d0b884369a2c96
JananiVelmurugan <jananise@gmail.com>
2019-03-07 11:46:56 +0530

    initial commit

---
+++ src/com/chainsys/dao/InsertDAO.java
@@ -0,0 +1,28 @@
+package com.chainsys.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.chainsys.model.Book;
+import com.chainsys.util.HibernateUtil;
+
+public class InsertDAO {
+	public static void main(String[] args) {
+		// Create student entity object
+		Book book = new Book();
+		book.setName("C");
+
+		// Create session factory object
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		// getting session object from session factory
+		Session session = sessionFactory.openSession();
+		// getting transaction object from session object
+		session.beginTransaction();
+
+		session.save(book);
+		System.out.println("Inserted Successfully");
+		session.getTransaction().commit();
+		session.close();
+		sessionFactory.close();
+	}
+}
